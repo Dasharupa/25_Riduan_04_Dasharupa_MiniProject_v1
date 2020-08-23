8b5288db21eb29fb68647473936024ec181b4b67
h3x1s <64124310+h3x1s@users.noreply.github.com>
2020-08-24 01:43:04 +0800

    Updated MiniProject
    Updated by Riduan

---
+++ 25_Riduan_04_Dasharupa_MiniProject/Assets/Scripts/HealthBar_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar_Script : MonoBehaviour
+{
+    private Image HealthBar;
+    public float CurrentHealth;
+    private float MaxHealth = 100f;
+    Move2D Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBar = GetComponent<Image>();
+        Player = FindObjectOfType<Move2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentHealth = Player.Health;
+        HealthBar.fillAmount = CurrentHealth / MaxHealth;
+    }
+
+}
